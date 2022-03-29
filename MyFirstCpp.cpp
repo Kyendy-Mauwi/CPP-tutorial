a8124980394688af164df57557d9859a1211c70a
Kyendy_Gurama <62172250+Kyendy-Mauwi@users.noreply.github.com>
2022-03-29 11:13:07 +0300

    Create MyFirstCpp.cpp

---
+++ MyFirstCpp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+/*iostream is the header file which contains all the functions of program like cout, cin etc. 
+and #include tells the preprocessor to include these header file in the program.*/
+
+using namespace std;
+/*
+std stands for standard thus this is a standard namespace.
+we cannot have variables, functions, etc with the same name. So to avoid those conflicts we use namespaces.
+So that means we use all the things with in “std” namespace.
+*/
+
+/*
+
+An int is a keyword that references an integer data type. 
+An int data type used with the main() function that indicates the function should return an integer value.
+*/
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+} 
