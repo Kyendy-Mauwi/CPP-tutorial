ed5bd909b1d86c7094bd868fc862aecee48ca35e
Kyendy_Gurama <62172250+Kyendy-Mauwi@users.noreply.github.com>
2022-03-29 22:11:14 +0300

    Update MyFirstCpp.cpp

--- MyFirstCpp.cpp
+++ MyFirstCpp.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
-/*iostream is the header file which contains all the functions of program like cout, cin etc. 
-and #include tells the preprocessor to include these header file in the program.*/
 
 using namespace std;
-/*
-std stands for standard thus this is a standard namespace.
+
+int main() {
+  cout << "Hey there!";
+  return 0;
+} 
+
+/* ```iostream``` is the header file which contains all the functions of program like cout, cin etc. 
+and #include tells the preprocessor to include these header file in the program.
+----------------------------------------------------------------------------------------------------------
+std stands for standard thus this is a ```standard namespace```.
 we cannot have variables, functions, etc with the same name. So to avoid those conflicts we use namespaces.
 So that means we use all the things with in “std” namespace.
-*/
-
-/*
+-----------------------------------------------------------------------------------------------------------
 An int is a keyword that references an integer data type. 
 An int data type used with the main() function that indicates the function should return an integer value.
 */
 
-int main() {
-  //print standard output
-  //cout is used to display the output to the standard output device.
-  cout << "Hey there!";
-  return 0;
-  //return 0 in the main function means that the program executed successfully.
-} 
+ /*print standard output.
+ ```cout``` is used to display the output to the standard output device.*/
 
-
+//return 0 in the main function means that the program executed successfully.
