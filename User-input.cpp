9d09a8963ee6b5be19ad16cf82f306e51caa7a49
Kyendy_Gurama <62172250+Kyendy-Mauwi@users.noreply.github.com>
2022-03-30 09:20:12 +0300

    Create User-input.cpp
    The user enters two number x and y and gets  x+y as the result.

---
+++ User-input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y; //```int``` is a fundamental variable type built into the compiler and used to define numeric variables.
+  int sum; //```array sum``` is the sum of all the elements of te array
+  cout << "Type a number: "; //```cout``` is an object of class outstream that reps. the standard output stream.
+  cin >> x; //```cin``` is used for taking any kind of data from the user.
+  cout << "Type another number: ";
+  cin >> y;
+  sum = x + y;
+  cout << "Sum is: " << sum;
+  return 0; // return 0 shows successful
+}
+
+/* ```#include <iostream>``` is a header file library that lets us work with input and output objects, such as cout.
+*/
